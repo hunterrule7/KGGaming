b76d86a77c17d35b64de1c572bfdf4a77571d974
Hunter Gulley <kryptongaming70@gmail.com>
2017-01-11 16:27:24 -0500

    Created a header for the main menu, created a settings window, added tab control, product test buttons, checkout list with product descriptions and prices.

--- KGGaming/MainMenu.xaml.cs
+++ KGGaming/MainMenu.xaml.cs
@@ -19,9 +19,53 @@ namespace KGGaming
     /// </summary>
     public partial class MainMenu : Window
     {
+        // Variables
+        List<storeProduct> inCartItems;
+        private double itemPrice;
+        private string formattedPrice;
+
         public MainMenu()
         {
             InitializeComponent();
+            inCartItems = new List<storeProduct>();
+        }
+
+        private void imgSettings_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            Settings settingsObj = new Settings();
+            settingsObj.Show();
+        }
+
+        private void btnItemTest_Click(object sender, RoutedEventArgs e)
+        {
+            itemPrice = 1.20;
+            formattedPrice = "$" + String.Format("{0:0.00}", itemPrice);
+            inCartItems.Add(new storeProduct() { Item = "Test Product", Price = formattedPrice });
+            cartList.ItemsSource = inCartItems;
+            cartList.Items.Refresh();
+            scrollToBottom();
+        }
+
+        public class storeProduct
+        {
+            public string Item { get; set; }
+            public string Price { get; set; }
+        }
+
+        private void btnItemTest2_Click(object sender, RoutedEventArgs e)
+        {
+            itemPrice = 1.00;
+            formattedPrice = "$" + String.Format("{0:0.00}", itemPrice);
+            inCartItems.Add(new storeProduct() { Item = "Test Product 2", Price = formattedPrice });
+            cartList.ItemsSource = inCartItems;
+            cartList.Items.Refresh();
+            scrollToBottom();
+        }
+
+        private void scrollToBottom()
+        {
+            cartList.SelectedIndex = cartList.Items.Count - 1;
+            cartList.ScrollIntoView(cartList.SelectedItem);
         }
     }
 }
